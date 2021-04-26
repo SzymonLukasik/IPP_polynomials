bf42e86d2c0519eeae1a9ee7cc39e2303cba309a
SzymonLukasik <szymon123lukasik@gmail.com>
2021-04-26 20:16:35 +0200

    Fixed some memory issues with PolyAdd and PolyMul. Formatted code.

--- src/safealloc.h
+++ src/safealloc.h
@@ -3,10 +3,10 @@
 
 #include <stdlib.h>
 
-void* SafeMalloc(size_t size);
+void *SafeMalloc(size_t size);
 
-void* SafeRealloc(void* ptr, size_t size);
+void *SafeRealloc(void *ptr, size_t size);
 
-void* SafeCalloc(size_t n, size_t size);
+void *SafeCalloc(size_t n, size_t size);
 
 #endif /* __SAFEALLOC_H__ */
