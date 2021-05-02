a7f32d70dee4ef4301819b684fc54be1c2af345f
SzymonLukasik <szymon123lukasik@gmail.com>
2021-05-02 13:41:09 +0200

    Minor code reformatting. Deleted not used functions from safealloc.

--- src/safealloc.c
+++ src/safealloc.c
@@ -2,22 +2,6 @@
 
 #include "safealloc.h"
 
-void* SafeMalloc(size_t size) {
-    void* p;
-    p = malloc(size);
-    if (p == NULL)
-        exit(1);
-    return p;
-}
-
-void* SafeRealloc(void* ptr, size_t size) {
-    void* p;
-    p = realloc(ptr, size);
-    if (p == NULL)
-        exit(1);
-    return p;
-}
-
 void* SafeCalloc(size_t n, size_t size) {
     void* p;
     p = calloc(n, size);
