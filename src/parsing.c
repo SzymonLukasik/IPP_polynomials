0f6a2b3b3a89124ceccb9823b9166157fb9bf80f
SzymonLukasik <szymon123lukasik@gmail.com>
2021-05-24 19:09:25 +0200

    Modified parsing - now require space after DEG_BY and AT

--- src/parsing.c
+++ src/parsing.c
@@ -332,7 +332,7 @@ static string GetParameter(string line, size_t line_length) {
  * @return Czy linię należy interpretować jako polecenie DEG_BY?
  */
 static bool IsDegBy(string line, Action *action, size_t line_length) {
-    if (strncmp(line, "DEG_BY", 6) == 0) {
+    if (strncmp(line, "DEG_BY ", 7) == 0) {
         string param = GetParameter(line, line_length);
         if (NumberTryToParse(param, VAR_IDX,
                              &action->spec.command.param.var_idx,
@@ -361,7 +361,7 @@ static bool IsDegBy(string line, Action *action, size_t line_length) {
  * @return Czy linię należy interpretować jako polecenie AT?
  */
 static bool IsAt(string line, size_t line_length, Action *action) {
-    if (strncmp(line, "AT", 2) == 0) {
+    if (strncmp(line, "AT ", 3) == 0) {
         string param = GetParameter(line, line_length);
         // Dokonujemy próby parsowania parametru
         if (NumberTryToParse(param, COEFF, &action->spec.command.param.x,
